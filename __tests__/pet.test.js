4c4278f1f2f2bbe152d201f8faf092a7b881daa8
Mario Cooper <mariocooper00@hotmail.com>
2023-08-02 20:12:14 +0100

    pets can now grow up

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -8,5 +8,18 @@ describe('constructor', () => {
     it('sets name property', () => {
         const pet = new Pet('Fido');
         expect(pet.name).toEqual('Fido');
-    })
+    });
+
+    it('has an inital age of 0', () => {
+        const pet = new Pet('Fido')
+        expect(pet.age).toEqual(0);
+    });
 });
+
+describe('growUp', () => {
+    it('increases age by 1', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        expect(pet.age).toEqual(1);
+    })
+})

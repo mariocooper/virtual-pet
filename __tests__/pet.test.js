c72aab98220c2659dc1a7cf5ddc781be204b7112
Mario Cooper <mariocooper00@hotmail.com>
2023-08-02 23:33:47 +0100

    growUp function edited & walk function added

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -21,5 +21,33 @@ describe('growUp', () => {
         const pet = new Pet('Fido');
         pet.growUp();
         expect(pet.age).toEqual(1);
+    });
+
+    it('increases hunger by 5', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        expect(pet.hunger).toEqual(5);
+    });
+
+    it('decreases fitness by 3', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        expect(pet.fitness).toEqual(7);
+    });
+});
+
+describe('walk', () => {
+    it('increases fitness by 4', () => {
+        const pet = new Pet('Fido');
+        pet.fitness = 4;
+        pet.walk();
+        expect(pet.fitness).toEqual(8);
+    });
+
+    it('increases fitness to a maximum of 10', () => {
+        const pet = new Pet('Fido');
+        pet.fitness = 9;
+        pet.walk();
+        expect(pet.fitness).toEqual(10);
     })
-})
+});
--- src/pet.js
+++ src/pet.js
@@ -1,10 +1,22 @@
 function Pet(name) {
     this.name = name
     this.age = 0
+    this.hunger = 0
+    this.fitness = 10
 }
 
 Pet.prototype.growUp = function() {
-    return this.age += 1;
+    this.age += 1;
+    this.hunger += 5;
+    this.fitness -= 3;
+}
+
+Pet.prototype.walk = function() {
+  if ((this.fitness + 4) <= 10) {
+    this.fitness += 4;
+  } else {
+    this.fitness = 10;
+  }
 }
 
 module.exports = Pet;
